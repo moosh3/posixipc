99981b8d40b6f8f6cfa5837e9eccc1fbc70bb0d2
aleccunningham <aleccunningham96@gmail.com>
2018-04-27 16:01:18 -0500

    Initial parent methods

--- mq.go
+++ mq.go
@@ -62,3 +62,11 @@ func (m *mq) openReadWrite() error { return nil }
 
 // O_CREAT
 func (m *mq) openCreate() error { return nil }
+
+func (m *mp) Send(mqd_t mqdes, msg_ptr uintptr, msg_len []byte, msg_prio int) error    { return nil }
+func (m *mq) Recieve(mqd_t mqdes, msg_ptr uintptr, msg_len []byte, int msg_prio) error { return nil }
+func (m *mq) Close(mqd_t mqdes) error                                                  { return nil }
+func (m *mq) Unlink(name string) error                                                 { return nil }
+func (m *mq) Notify(mqd_t mqdes, notification sigevent) error                          { return nil }
+func (m *mq) SetAttr(mqd_t mqdes, mqstat mq_attr, omqstat mq_attr) error               { return nil }
+func (m *mq) GetAttr(mqd_t mqdes, mqstat mq_attr, omqstat mq_attr) error               { return nil }
--- semaphore.go
+++ semaphore.go
@@ -47,10 +47,18 @@ type isemaphore interface {
 	Post(*sem) error
 }
 
-func (s *sem) Open()
+func (s *sem) Open(name string, oflag int) error {
+
+}
 
 // O_EXCL
 func (s *sem) openReadWrite() error { return nil }
 
 // O_CREAT
 func (s *sem) openCreate() error { return nil }
+
+func (s *sem) Init(sem_t *sem, pshared int, value int) error { return nil }
+func (s *sem) Close(sem_t *sem) error                        { return nil }
+func (s *sem) Unlink(name *string) error                     { return nil }
+func (s *sem) Destroy(sem_t *sem) error                      { return nil }
+func (s *sem) GetValue(sem_t *sem, sval *int) error          { return nil }
